64bae4066fda701eae8487883a29e90f91c49e1f
darinmorrison <darinmorrison@users.noreply.github.com>
2021-01-13 08:57:16 +0000

    deploy: 7d838543674a753f2255d08a57a05791eb3d63ed

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["lspower"] = {"name":"","dirs":[{"name":"jsonrpc","files":["error.rs","pending.rs"]}],"files":["client.rs","codec.rs","jsonrpc.rs","lib.rs","service.rs","transport.rs"]};
+sourcesIndex["lspower"] = {"name":"","dirs":[{"name":"jsonrpc","files":["error.rs","pending.rs"]}],"files":["client.rs","codec.rs","jsonrpc.rs","lib.rs","server.rs","service.rs","transport.rs"]};
 createSourceSidebar();
