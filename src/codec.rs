d72150c2c3afc4ddf6e54402a63987eb39d4cc04
silvanshade <silvanshade@users.noreply.github.com>
2021-03-01 22:30:42 -0700

    Fix a bug decoding long messages; add comments

--- src/codec.rs
+++ src/codec.rs
@@ -128,16 +128,26 @@ impl<T: serde::de::DeserializeOwned> Decoder for LanguageServerCodec<T> {
             return Ok(None);
         }
 
+        // The potential error returned by httparse
         let mut http_error = None;
+        // The value of the "Content-Length" header
         let mut content_len = None;
+        // The total length of the parsed http headers
         let mut headers_len = None;
 
         {
+            // Placeholder used for parsing headers into
             let dst = &mut [httparse::EMPTY_HEADER; 2];
+
+            // Parse the headers and try to extract values for the previous mut vars
             match httparse::parse_headers(src, dst) {
+                // A complete set of headers was parsed succesfully
                 Ok(httparse::Status::Complete((offset, headers))) => {
+                    // If some headers were parsed successefully, set the headers length
                     headers_len = Some(offset);
+                    // Scan through the headers
                     for header in headers {
+                        // If the "Content-Length" header is found, parse the value as a usize
                         if header.name == "Content-Length" {
                             let value = std::str::from_utf8(header.value)?;
                             let value = value.parse::<usize>().map_err(|_| ParseError::InvalidLength)?;
@@ -145,35 +155,55 @@ impl<T: serde::de::DeserializeOwned> Decoder for LanguageServerCodec<T> {
                         }
                     }
                 },
+                // No errors occurred during parsing yet but no complete set of headers were parsed
                 Ok(httparse::Status::Partial) => {
+                    // Return None and wait for more input
                     return Ok(None);
                 },
+                // An error occurred during parsing of the headers
                 Err(error) => {
                     http_error = Some(error);
                 },
             }
         }
 
+        // Headers were parsed (but "Content-Length" wasn't found) or an error occurred during parsing
         if content_len.is_none() || http_error.is_some() {
+            // Maybe there are garbage prefix bytes so try to scan ahead for "Content-Length" to recover
             if let Some(offset) = twoway::find_bytes(src, b"Content-Length") {
                 src.advance(offset);
             }
+            // Then handle the conditions that caused decoding to fail ...
             if let Some(http_error) = http_error {
+                // ... there was an error parsing the headers
                 return Err(ParseError::Httparse(http_error));
             } else {
+                // ... there was no "Content-Length" header found
                 return Err(ParseError::MissingHeader);
             }
         }
 
+        // The total length of the headers and the content
         let delta;
+
+        // The result of parsing the content
         let result = if let (Some(headers_len), Some(content_len)) = (headers_len, content_len) {
             delta = headers_len + content_len;
-            let msg = &src[headers_len .. delta];
-            let msg = std::str::from_utf8(msg)?;
-            log::trace!("<- {}", msg);
-            match serde_json::from_str(msg) {
-                Ok(parsed) => Ok(Some(parsed)),
-                Err(err) => Err(err.into()),
+
+            // First, ensure that the source bytes is long enough for us to decode the full content ...
+            if src.len() < delta {
+                // ... otherwise return None and wait for more input
+                return Ok(None);
+            } else {
+                let msg = &src[headers_len .. delta];
+                let msg = std::str::from_utf8(msg)?;
+
+                log::trace!("<- {}", msg);
+
+                match serde_json::from_str(msg) {
+                    Ok(parsed) => Ok(Some(parsed)),
+                    Err(err) => Err(err.into()),
+                }
             }
         } else {
             unreachable!()
